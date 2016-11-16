ecda11aca830fd65c5634e1d2ee777003fcf5bf9
Carl Vincent Baydo <cvbaydo@yahoo.com.ph>
2016-11-16 15:23:17 +0800

    Add showCompleted and searchText reducers

---
+++ app/reducers/reducers.jsx
@@ -0,0 +1,18 @@
+export var searchTextReducer = (state = '', action) => {
+  switch (action.type) {
+    case 'SET_SEARCH_TEXT':
+      return action.searchText;
+    default:
+      return state;
+  };
+};
+
+export var showCompletedReducer = (state = false, action) => {
+  switch (action.type) {
+    case 'TOGGLE_SHOW_COMPLETED':
+      return !state;
+      // return state === false ? true : false;
+    default:
+      return state;
+  };
+};
